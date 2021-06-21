0e139d567a516975e8cb7472f421bbd079f31d58
dev cat <81799160+devcatme@users.noreply.github.com>
2021-06-21 16:06:59 +0700

    cheerio
    code cheerio html

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const cheerio = require('cheerio');
+
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
+  const cheerio = require('cheerio');
+  const datahtml = cheerio.load('<h2 class="title">Hello world Dev cat</h2>');
+
+  datahtml('h2.title').text('Hello there!');
+  datahtml('h2').addClass('welcome');
+
+  //datahtml.html();
+
+
   res.send('Hello World test1')
+
 })
 
 app.listen(port, () => {
